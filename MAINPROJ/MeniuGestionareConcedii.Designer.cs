1a55120e812af698977c253b017b4bb787470e46
Denisa Marica <denisa.marica@totalsoft.ro>
2022-08-30 16:56:58 +0300

    DenisaMeniuConcedii

--- MAINPROJ/MeniuGestionareConcedii.Designer.cs
+++ MAINPROJ/MeniuGestionareConcedii.Designer.cs
@@ -48,11 +48,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(312, 134);
+            this.dataGridView1.Location = new System.Drawing.Point(207, 107);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(525, 290);
             this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // MeniuGestionareConcedii
             // 
--- MAINPROJ/MeniuGestionareConcedii.cs
+++ MAINPROJ/MeniuGestionareConcedii.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,27 @@ namespace MAINPROJ
         public MeniuGestionareConcedii()
         {
             InitializeComponent();
+            showTable();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
+            private void showTable()
+            {
+                string constring = @"Data Source=ts2112\SQLEXPRESS;Initial Catalog=PrisonBreak;Persist Security Info=True;User ID=internship2022;Password=int";
+                using (SqlConnection con = new SqlConnection(constring))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT a.Nume, a.Prenume, f.Nume AS Functie, c.Data_inceput, c.Data_sfarsit, DATEDIFF(weekday,c.Data_inceput, c.Data_sfarsit) AS Perioada, tc.Nume AS TipConcediu\r\nFROM Angajat a\r\nJOIN Concediu c on a.Id=c.angajatId\r\nJOIN TipConcediu tc on c.TipConcediuId = tc.Id\r\nJOIN Functie f on f.Id = a.IdFunctie", con))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            using (DataTable dt = new DataTable())
+                            {
+                                sda.Fill(dt);
+                                dataGridView1.DataSource = dt;
+                            }
+                        }
+                    }
+                }
+            }
     }
 }
