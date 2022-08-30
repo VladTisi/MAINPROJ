7011344fb3c9010a1787df3fa56924e83c18bde5
Vladut Mielu <vladut.mielu@totalsoft.ro>
2022-08-30 17:04:35 +0300

    gion

--- MAINPROJ/Program.cs
+++ MAINPROJ/Program.cs
@@ -17,7 +17,7 @@ namespace MAINPROJ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CerereConcediu());
+            Application.Run(new MeniuGestionareConcedii());
         }
     }
 }
