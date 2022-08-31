29c4523163298a73fb9940c6bd0c26bb71ad39da
Vlad Tisianu <vlad.tisianu@totalsoft.ro>
2022-08-31 10:29:59 +0300

    S

--- MAINPROJ/Program.cs
+++ MAINPROJ/Program.cs
@@ -17,7 +17,7 @@ namespace MAINPROJ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MeniuGestionareConcedii());
+            Application.Run(new ConcediiPersonale());
         }
     }
 }
