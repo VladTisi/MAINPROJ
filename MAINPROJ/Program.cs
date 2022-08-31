030bfe62557f02c18455625d55086bb86fd6edda
CosminTotal <cosmin.gostin@totalsoft.ro>
2022-08-31 10:22:10 +0300

    cossss

--- MAINPROJ/Program.cs
+++ MAINPROJ/Program.cs
@@ -1,4 +1,4 @@
-﻿using PrisonBreakProj;
+﻿using MAINPROJ;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +17,7 @@ namespace MAINPROJ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
-            Application.Run(new MeniuNavigare());
+            Application.Run(new LogAuten());
         }
     }
 }
