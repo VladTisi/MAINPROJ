03eb9c38da4ea535a73ef1cd696c0dc83cb89828
Vlad Tisianu <vlad.tisianu@totalsoft.ro>
2022-09-01 13:16:31 +0300

    Vlad.ForgotPassword

--- MAINPROJ/Class1.cs
+++ MAINPROJ/Class1.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MAINPROJ
 {
-    internal class Class1
+    public static class Class1
     {
+        public static void sendMail(string subject,string body,string to)
+        {
+            var from = "vlad.tisianu@totalsoft.ro";
+            var username = "vlad.tisianu@totalsoft.ro"; 
+            var password = "Thew0lf2234*"; 
+            var host = "mailer14.totalsoft.local";
+            var port = 587;
+
+            var client = new SmtpClient(host, port)
+            {
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = true
+            };
+
+            client.Send(from, to, subject, body);
+
+            Console.WriteLine("Email sent");
+        }
     }
 }
--- MAINPROJ/LogAuten.cs
+++ MAINPROJ/LogAuten.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using static MAINPROJ.Class1;
+using System.Web.Security;
+using Microsoft.VisualBasic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
 namespace MAINPROJ
 {
     public partial class LogAuten : Form
@@ -192,6 +197,55 @@ namespace MAINPROJ
 
         private void autemail_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            DialogResult dialogResult = MessageBox.Show("Doriti sa resetati parola?", "Resetare parola", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                OleDbConnection conn = Common.GetConnection();
+                Random r = new Random();
+                int x = r.Next(100000, 999999);
+                Console.WriteLine(x);
+                string email = logmail.Text;
+                cmd = new OleDbCommand($"SELECT Parola FROM Login WHERE Email='{email}'");
+                cmd.Connection = conn;
+                conn.Open();
+                string password = (string)cmd.ExecuteScalar();
+                if (!String.IsNullOrEmpty(password))
+                {
+                    Class1.sendMail("Mail resetare parola", $"Ati solicitat schimbarea parolei. Introduceti codul:{x}. Daca nu ati fost dumneavoastra, ignorati acest mail. ", "denisa.marica@totalsoft.ro");
+                    String s = Interaction.InputBox("Introduceti codul de validare primit pe email", "Cod de validare", "000000");
+                    if (Int32.Parse(s)==x)
+                    {
+                        string selectId = $"SELECT AngajatId FROM Login WHERE Email='{email}'";
+                        cmd.CommandText = selectId;
+                        int angajatId = (int) cmd.ExecuteScalar();
+                        string generated_pass = Membership.GeneratePassword(8, 0);
+                        Console.WriteLine(generated_pass);
+                        string updatePasswordQuery = $"UPDATE Login SET Parola='{generated_pass}' WHERE AngajatId={angajatId}";
+                        cmd.CommandText=updatePasswordQuery;
+                        cmd.ExecuteNonQuery();
+                        Class1.sendMail("Parola temporara", $"Parola dumneavoasta temporara este: {generated_pass}", "denisa.marica@totalsoft.ro");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid code");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nu exista un cont cu acest email");
+                }
+                conn.Close();
+
+
+            }
+
 
         }
     }
