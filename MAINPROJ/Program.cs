918cb9c42d4dcef6816b0f468aa7883a70950a8e
CosminTotal <cosmin.gostin@totalsoft.ro>
2022-09-01 11:38:07 +0300

    poza

--- MAINPROJ/Program.cs
+++ MAINPROJ/Program.cs
@@ -17,7 +17,7 @@ namespace MAINPROJ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MeniuNavigare(5));
+            Application.Run(new MeniuNavigare(2));
             
         }
     }
