5c4bc8bda87d7478c48501f321b473d8435f74b3
Vlad Tisianu <vlad.tisianu@totalsoft.ro>
2022-09-06 14:06:48 +0300

    s

---
+++ MAINPROJ/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomProj.Models
+{
+    public partial class Login
+    {
+        public Login()
+        {
+            Angajats = new HashSet<Angajat>();
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; } 
+        public string Parola { get; set; } 
+        public int? AngajatId { get; set; }
+
+        public virtual Angajat Angajat { get; set; }
+        public virtual ICollection<Angajat> Angajats { get; set; }
+    }
+}
---
+++ MAINPROJ/Models/TipConcediu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomProj.Models
+{
+    public partial class TipConcediu
+    {
+        public TipConcediu()
+        {
+            Concedius = new HashSet<Concediu>();
+        }
+        public int Id { get; set; }
+        public string Nume { get; set; } 
+        public string Cod { get; set; } 
+
+        public virtual ICollection<Concediu> Concedius { get; set; }
+    }
+}
